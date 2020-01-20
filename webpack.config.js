dab0d75246cd7ceba2c2b892bff0cb9b967e7ea5
Oskar <oskar.maslek@gmail.com>
2020-01-20 14:25:21 +0100

    Config webpack and plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ],
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader', // Creates `style` nodes from JS strings
+          'css-loader', // Translates CSS into CommonJS
+          'sass-loader', // Compiles Sass to CSS
+        ],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'index.html'
+  })]
 };
